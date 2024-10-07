d92c40df5b9ed757fda39ba149dc6305541cd41e
BGW-10 <barry.walker1210@gmail.com>
2024-10-07 13:20:26 -0400

    New simulation launch and description files

---
+++ src/trr_robot/launch/trr_robot_gz_spawn.launch.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+
+from os.path import join
+from xacro import parse, process_doc
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+
+def get_xacro_to_doc(xacro_file_path, mappings):
+    doc = parse(open(xacro_file_path))
+    process_doc(doc, mappings=mappings)
+    return doc
+
+def generate_launch_description():
+   
+    trr_robot_path = get_package_share_directory("trr_robot")
+    position_x = LaunchConfiguration("position_x")
+    position_y = LaunchConfiguration("position_y")
+    orientation_yaw = LaunchConfiguration("orientation_yaw")
+    camera_enabled = LaunchConfiguration("camera_enabled", default=True)
+    stereo_camera_enabled = LaunchConfiguration("stereo_camera_enabled", default=False)
+    two_d_lidar_enabled = LaunchConfiguration("two_d_lidar_enabled", default=True)
+    odometry_source = LaunchConfiguration("odometry_source")
+
+    # robot_description_content = get_xacro_to_doc(
+    #     join(trr_robot_path, "description", "trr_robot.xacro"),
+    #     {"sim_gz": "true",
+    #      "two_d_lidar_enabled": "true",
+    #      "conveyor_enabled": "false",
+    #      "camera_enabled": "true"
+    #     }
+    # ).toxml()
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        parameters=[
+                    {'robot_description': Command( \
+                    ['xacro ', join(trr_robot_path, 'description/trr_robot.xacro'),
+                    ' camera_enabled:=', camera_enabled,
+                    ' stereo_camera_enabled:=', stereo_camera_enabled,
+                    ' two_d_lidar_enabled:=', two_d_lidar_enabled,
+                    ' odometry_source:=', odometry_source,
+                    ' sim_gz:=', "true"
+                    ])}],
+        remappings=[
+            ('/joint_states', 'trr_robot/joint_states'),
+        ]
+    )
+
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-topic", "/robot_description",
+            "-name", "trr_robot",
+            "-allow_renaming", "true",
+            "-z", "0.28",
+            "-x", position_x,
+            "-y", position_y,
+            "-Y", orientation_yaw
+        ]
+    )
+
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry",
+            "/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
+            "/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan",
+            "/depth_camera@sensor_msgs/msg/Image[gz.msgs.Image",
+            "depth_camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
+            "/depth_camera/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
+            "/world/default/model/trr_robot/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model"
+        ],
+        remappings=[
+            ('/world/default/model/trr_robot/joint_state', 'trr_robot/joint_states'),
+            ('/odom', 'trr_robot/odom'),
+            ('/scan', 'trr_robot/scan'),
+            ('/depth_camera', 'trr_robot/depth_camera'),
+            ('/cmd_vel', 'trr_robot/cmd_vel'),
+            ('depth_camera/camera_info', 'trr_robot/depth_camera/camera_info'),
+            ('/depth_camera/points', 'trr_robot/depth_camera/points'),
+        ]
+    )
+
+    transform_publisher = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments = ["--x", "0.0",
+                    "--y", "0.0",
+                    "--z", "0.0",
+                    "--yaw", "0.0",
+                    "--pitch", "0.0",
+                    "--roll", "0.0",
+                    "--frame-id", "depth_camera",
+                    "--child-frame-id", "trr_robot/base_footprint/depth_camera"]
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument("camera_enabled", default_value = camera_enabled),
+        DeclareLaunchArgument("stereo_camera_enabled", default_value = stereo_camera_enabled),
+        DeclareLaunchArgument("two_d_lidar_enabled", default_value = two_d_lidar_enabled),
+        DeclareLaunchArgument("position_x", default_value="0.0"),
+        DeclareLaunchArgument("position_y", default_value="0.0"),
+        DeclareLaunchArgument("orientation_yaw", default_value="0.0"),
+        DeclareLaunchArgument("odometry_source", default_value="world"),
+        robot_state_publisher,
+        gz_spawn_entity, transform_publisher, gz_ros2_bridge
+    ])
