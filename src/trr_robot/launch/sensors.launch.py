b26cc31e622b91d546ec9adb4d3ce73e0f9805ba
BGW-10 <barry.walker1210@gmail.com>
2024-10-20 20:34:34 -0400

    Launch file for physical lidar and realsense

--- src/trr_robot/launch/sensors.launch.py
+++ src/trr_robot/launch/sensors.launch.py
@@ -3,9 +3,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.actions import OpaqueFunction
 from launch.actions import SetLaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
@@ -14,6 +15,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     pkg_trr = get_package_share_directory('trr_robot')
+    pkg_realsense = get_package_share_directory('realsense2_camera')
 
     launch_description = LaunchDescription([
         DeclareLaunchArgument(
@@ -35,6 +37,15 @@ def generate_launch_description():
         output='screen',
         parameters=[LaunchConfiguration('param')]
     )
-
     launch_description.add_action(hokuyo_node)
+
+    realsense_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_realsense, "launch", "rs_launch.py")),
+        launch_arguments={
+            "depth_module.depth_profile" : "1280x720x30",
+            "pointcloud.enable" : "true"
+        }.items()
+    )
+    launch_description.add_action(realsense_node)
+    
     return launch_description
