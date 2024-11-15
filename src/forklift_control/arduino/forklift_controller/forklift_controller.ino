5907231f67ec3edeafa61896717aee2188d6267c
BGW-10 <barry.walker1210@gmail.com>
2024-11-15 00:09:25 -0500

    Initial arduino forklift control implementation

---
+++ src/forklift_control/arduino/forklift_controller/forklift_controller.ino
@@ -0,0 +1,130 @@
+#include <PinChangeInterrupt.h>
+
+constexpr int MOTOROUT = 11;      // Motor PWM out
+constexpr int DIRECTION = 12;     // Direction pin out
+
+constexpr int TOPSWITCH = 9;      // Top sensor switch
+constexpr int BOTTOMSWITCH = 10;  // Bottom sensor switch
+
+constexpr int ENCODER1 = 2;       // First encoder pin
+constexpr int ENCODER2 = 3;       // Second encoder pin
+
+constexpr int MOTORSPEED = 100;   // Set the motor speed
+
+int encoderCount = 0;             // Encoder count
+int bottomToTop = -1;             // Number of encoder ticks from bottom to top of forklift. Gets set after homing.
+
+void updateEncoderCount();
+void topReached();
+void bottomReached();
+void homingProcedure();
+
+// Setup before main loop.
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(TOPSWITCH, INPUT);
+  pinMode(ENCODER1, INPUT);
+  pinMode(ENCODER2, INPUT);
+
+  pinMode(MOTOROUT, OUTPUT);
+  pinMode(DIRECTION, OUTPUT);
+
+  attachPCINT(digitalPinToPCINT(ENCODER1), updateEncoderCount, RISING);
+
+  attachPCINT(digitalPinToPCINT(TOPSWITCH), topReached, RISING);
+  attachPCINT(digitalPinToPCINT(BOTTOMSWITCH), bottomReached, RISING);
+}
+
+// Main program loop.
+void loop() {
+  // Reenabling the end interrupts.
+  enablePCINT(digitalPinToPCINT(TOPSWITCH));
+  enablePCINT(digitalPinToPCINT(BOTTOMSWITCH));
+
+  // Wait for a serial command.
+  digitalWrite(LED_BUILTIN, HIGH);
+  while (Serial.available() == 0);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // Read the serial command.
+  String input = Serial.readString();
+  char func = input[0];
+
+  // Interpret the serial command.
+  switch (func) {
+    // "h" runs the homing procedure.
+    case 'h':
+      homingProcedure();
+      break;
+    // "p [0.0-1.0]" sets the fork position.
+    case 'p':
+      int spaceIndex = input.indexOf(' ');
+      String numberStr = input.substring(spaceIndex + 1);
+      float val = numberStr.toFloat();
+      toPos(val);
+      break;
+    default: 
+      break;
+  }
+}
+
+// Update encoders on interrupt.
+void updateEncoderCount() {
+  // If ENCODER1 is high by the time it gets to ENCODER2 then it's moving clockwise.
+  if (digitalRead(ENCODER1) == HIGH && digitalRead(ENCODER2) == LOW) {
+    encoderCount++;
+  } else {
+    encoderCount--;
+  }
+}
+
+// Runs the homing procedure
+void homingProcedure() {
+  // Lower the forklift as far as possible.
+  digitalWrite(DIRECTION, 0);
+  analogWrite(MOTOROUT, MOTORSPEED);
+  while (digitalRead(BOTTOMSWITCH) == LOW);
+  encoderCount = 0;
+
+  // Raise the forklift as high as possible. Count the amount of ticks.
+  digitalWrite(DIRECTION, 1);
+  analogWrite(MOTOROUT, MOTORSPEED);
+  while (digitalRead(TOPSWITCH) == LOW);
+  bottomToTop = encoderCount;
+}
+
+// Moves the forklift to specified position.
+// 1.0: top, 0.5: middle, 0.0: bottom
+void toPos(float val) {
+  // Doesn't run if forklift isn't homed
+  if (bottomToTop == -1) {
+    return;
+  }
+
+  // Calculate the target amount of ticks and move the forks there
+  int target = round(bottomToTop * val);
+  int direction = encoderCount > target ? 1 : 0;
+  digitalWrite(DIRECTION, direction);
+  analogWrite(MOTOROUT, MOTORSPEED);
+  if (direction == 0) {
+    while (encoderCount > target);
+  } else {
+    while (encoderCount < target);
+  }
+}
+
+// Triggers when the top of the forklift is reached
+void topReached() {
+  analogWrite(MOTOROUT, 0);
+  disablePCINT(digitalPinToPCINT(TOPSWITCH));
+  enablePCINT(digitalPinToPCINT(BOTTOMSWITCH));
+}
+
+// Triggers when the bottom of the forklift is reached
+void bottomReached() {
+  analogWrite(MOTOROUT, 0);
+  disablePCINT(digitalPinToPCINT(BOTTOMSWITCH));
+  enablePCINT(digitalPinToPCINT(TOPSWITCH));
+}
