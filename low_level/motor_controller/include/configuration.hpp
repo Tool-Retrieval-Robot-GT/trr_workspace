1fbb1fd551b68f91af1c10d771c63749fd670ec7
BGW-10 <barry.walker1210@gmail.com>
2024-11-18 11:33:35 -0500

    Configuring pins

--- low_level/motor_controller/include/configuration.hpp
+++ low_level/motor_controller/include/configuration.hpp
@@ -8,27 +8,27 @@
 // -----------------------------------------------------------------------------
 
 // GPIO pin configuration for the right motor.
-#define GPIO_MOTOR_RIGHT_EN 10  // Must be a PWM pin
-#define GPIO_MOTOR_RIGHT_IN1 6
-#define GPIO_MOTOR_RIGHT_IN2 7
-#define GPIO_MOTOR_RIGHT_ENCODER_A 3  // Must be a hardware interrupt pin
-#define GPIO_MOTOR_RIGHT_ENCODER_B 5
+#define GPIO_MOTOR_RIGHT_EN         9   // Must be a PWM pin
+#define GPIO_MOTOR_RIGHT_IN1        12
+#define GPIO_MOTOR_RIGHT_IN2        11
+#define GPIO_MOTOR_RIGHT_ENCODER_A  A4  // Must be a hardware interrupt pin
+#define GPIO_MOTOR_RIGHT_ENCODER_B  A5
 
 // GPIO pin configuration for the left motor.
-#define GPIO_MOTOR_LEFT_EN 11  // Must be a PWM pin
-#define GPIO_MOTOR_LEFT_IN1 8
-#define GPIO_MOTOR_LEFT_IN2 9
-#define GPIO_MOTOR_LEFT_ENCODER_A 2  // Must be a hardware interrupt pin
-#define GPIO_MOTOR_LEFT_ENCODER_B 4
+#define GPIO_MOTOR_LEFT_EN          5   // Must be a PWM pin
+#define GPIO_MOTOR_LEFT_IN1         7
+#define GPIO_MOTOR_LEFT_IN2         8
+#define GPIO_MOTOR_LEFT_ENCODER_A   A2  // Must be a hardware interrupt pin
+#define GPIO_MOTOR_LEFT_ENCODER_B   A6
 
 // -----------------------------------------------------------------------------
 // ----------------------------| Motor Configuration |--------------------------
 // -----------------------------------------------------------------------------
 
 // Motor and encoder configuration settings.
-#define WHEEL_RADIUS 0.0339  // Wheel radius in meters
-#define ENCODER_TICKS_PER_REVOLUTION 490
-#define DIST_BETWEEN_WHEELS 0.20  // Distance between wheels in meters
+#define WHEEL_RADIUS 0.04  // Wheel radius in meters
+#define ENCODER_TICKS_PER_REVOLUTION 550
+#define DIST_BETWEEN_WHEELS 0.47  // Distance between wheels in meters
 
 // PID gains
 #define MOTOR_DRIVER_PID_KP 200.0
