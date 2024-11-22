5b8001cd7326ba43a6f548c35dd2d23d732f5ba6
Tyler <tylerreagan25@gmail.com>
2024-11-22 17:37:24 +0000

    Fixed some things.

--- src/qr_recognition/scripts/qr_reader.py
+++ src/qr_recognition/scripts/qr_reader.py
@@ -53,14 +53,11 @@ class QRReader(Node):
             self.QRTop = self.decodeMsg.rect[1]
             self.QRWidth = self.decodeMsg.rect[2]
             self.QRHeight = self.decodeMsg.rect[3]
-            self.isQRCodeCentered()
-            self.driveToQRCode()
+            if(self.isQRCodeCentered() == True):
+                self.driveToQRCode()
+                self.raiseForkLift()
         elif(not self.decodeMsg and self.imageFound == True):
             self.imageFound = False
-
-
-
-        cv2.imshow("Camera", currentImage)
         cv2.waitKey(3)
 
     # This function will publish the image data to external nodes
@@ -82,23 +79,27 @@ class QRReader(Node):
             # Determine which axis needs to change
             topVal = self.cameraCenter[1] - QRCodeCenter[1]
             leftVal = self.cameraCenter[0] - QRCodeCenter[0]
-            incrementMsg = Twist()
+            driveMsg = Twist()
+            raiseMsg = Float32()
             if(self.QRTop != topVal):
                 if (self.QRTop > topVal):
-                    # Move down
+                    # Move Up
+                    raiseMsg.data = 0.05
+                    self.forkliftPublisher.publish(raiseMsg)
                 elif (self.QRTop < topVal):
                     # Move down
+                    raiseMsg.data = -0.05
+                    self.forkliftPublisher.publish(raiseMsg)
             elif(self.QRLeft != leftVal):
                 if(self.QRLeft > leftVal):
                     # Move left
-                    incrementMsg.linear.x = -0.5
-                    while(self.QRLeft > leftVal):
-                        self.publisher.publish(incrementMsg)
+                    driveMsg.linear.x = -0.5
+                    self.movementPublisher.publish(driveMsg)
                 elif(self.QRLeft < leftVal):
                     # Move Right
-                    incrementMsg.linear.x = 0.5
-                    while(self.QRLeft < leftVal):
-                        self.publisher.publish(incrementMsg)
+                    driveMsg.linear.x = 0.5
+                    self.movementPublisher.publish(driveMsg)
+            return False
 
     def driveToQRCode(self):
         moveForewardMsg = Twist()
@@ -106,11 +107,11 @@ class QRReader(Node):
         while(self.QRWidth <=  3 * self.cameraWidth / 4 and self.QRHeight <= 3 * self.cameraHeight / 4):
             self.movementPublisher.publish(moveForewardMsg)
         # Once the QR code is taking up most of the frame go foreward an appoximate distance
-        endTime = time.time() + 5 # Run for 5 seconds
+        endTime = time.time() + 3 # Run for 3 seconds
         while(currentTime < endTime):
             currentTime = time.time()
 
-    def controlForkLift(self):
+    def raiseForkLift(self):
         forkLiftMsg = Float32()
         forkLiftMsg.data = 1.0
         # Raise the bin to max height
